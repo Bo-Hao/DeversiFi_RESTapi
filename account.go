dc30fe63ee7f72352e54afbc0674e9d2f7d33933
彭柏豪 <perngbohao@pengbaihaodeMacBook-Pro.local>
2022-02-11 22:20:08 +0800

    bohao added

--- account.go
+++ account.go
@@ -1,6 +1,8 @@
 package dvfapi
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"time"
@@ -36,7 +38,7 @@ func (p *Client) GetFeeRate() (result *GetFeeRateResponse, err error) {
 	return result, nil
 }
 
-type GetBalanceResponse struct {
+type GetBalanceResponse []struct {
 	Balance       int    `json:"balance"`
 	ActiveBalance int    `json:"activeBalance"`
 	ID            string `json:"_id"`
@@ -68,5 +70,182 @@ func (p *Client) GetBalance() (result *GetBalanceResponse, err error) {
 	if err != nil {
 		return nil, err
 	}
+	return result, nil
+}
+
+type GetUserConfigResponse struct {
+	Dvf struct {
+		StarkExVersion                         string  `json:"starkExVersion"`
+		DefaultFeeRate                         float64 `json:"defaultFeeRate"`
+		DefaultFeeRateSwap                     float64 `json:"defaultFeeRateSwap"`
+		DeversifiAddress                       string  `json:"deversifiAddress"`
+		StarkExContractAddress                 string  `json:"starkExContractAddress"`
+		WithdrawalBalanceReaderContractAddress string  `json:"withdrawalBalanceReaderContractAddress"`
+		StarkExTransferRegistryContractAddress string  `json:"starkExTransferRegistryContractAddress"`
+		RegistrationAndDepositInterfaceAddress string  `json:"registrationAndDepositInterfaceAddress"`
+		AMMfactoryAddress                      string  `json:"AMMfactoryAddress"`
+		AMMrouterAddress                       string  `json:"AMMrouterAddress"`
+		BridgeConfigPerChain                   struct {
+			MaticPos struct {
+				ContractAddress       string  `json:"contractAddress"`
+				WithdrawalFeeRatio    int     `json:"withdrawalFeeRatio"`
+				MaxWithdrawalRatio    float64 `json:"maxWithdrawalRatio"`
+				MaxTotalUsdInContract int     `json:"maxTotalUsdInContract"`
+			} `json:"MATIC_POS"`
+		} `json:"bridgeConfigPerChain"`
+		ExchangeSymbols      []string `json:"exchangeSymbols"`
+		DlmMarkets           []string `json:"dlmMarkets"`
+		TempStarkVaultID     int      `json:"tempStarkVaultId"`
+		MinDepositUSDT       int      `json:"minDepositUSDT"`
+		AuthVersion          int      `json:"authVersion"`
+		DisableLP            bool     `json:"disableLP"`
+		DeversifiStarkKeyHex string   `json:"deversifiStarkKeyHex"`
+	} `json:"DVF"`
+	TokenBalancesHistory []string `json:"tokenBalancesHistory"`
+	TradingRewards       struct {
+		WeeklyAmount    int      `json:"weeklyAmount"`
+		RewardToken     string   `json:"rewardToken"`
+		ExcludedMarkets []string `json:"excludedMarkets"`
+	} `json:"tradingRewards"`
+	AmmPools      map[string]AmmPools      `json:"ammPools"`
+	TokenRegistry map[string]TokenRegistry `json:"tokenRegistry"`
+	IsRegistered  bool                     `json:"isRegistered"`
+	EthAddress    string                   `json:"ethAddress"`
+}
+
+// Returns the DeversiFi application and user configuration details.
+func (p *Client) GetUserConfig() (result *GetUserConfigResponse, err error) {
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]string)
+	nonce := time.Now().Unix()
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+	params["token"] = "ETH"
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/r/getUserConf", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type RegisterResponse struct {
+	Dvf struct {
+		StarkExVersion                         string  `json:"starkExVersion"`
+		DefaultFeeRate                         float64 `json:"defaultFeeRate"`
+		DefaultFeeRateSwap                     float64 `json:"defaultFeeRateSwap"`
+		DeversifiAddress                       string  `json:"deversifiAddress"`
+		StarkExContractAddress                 string  `json:"starkExContractAddress"`
+		WithdrawalBalanceReaderContractAddress string  `json:"withdrawalBalanceReaderContractAddress"`
+		StarkExTransferRegistryContractAddress string  `json:"starkExTransferRegistryContractAddress"`
+		RegistrationAndDepositInterfaceAddress string  `json:"registrationAndDepositInterfaceAddress"`
+		AMMfactoryAddress                      string  `json:"AMMfactoryAddress"`
+		AMMrouterAddress                       string  `json:"AMMrouterAddress"`
+		BridgeConfigPerChain                   struct {
+			MaticPos struct {
+				ContractAddress       string  `json:"contractAddress"`
+				WithdrawalFeeRatio    int     `json:"withdrawalFeeRatio"`
+				MaxWithdrawalRatio    float64 `json:"maxWithdrawalRatio"`
+				MaxTotalUsdInContract int     `json:"maxTotalUsdInContract"`
+			} `json:"MATIC_POS"`
+		} `json:"bridgeConfigPerChain"`
+		ExchangeSymbols      []string `json:"exchangeSymbols"`
+		DlmMarkets           []string `json:"dlmMarkets"`
+		TempStarkVaultID     int      `json:"tempStarkVaultId"`
+		MinDepositUSDT       int      `json:"minDepositUSDT"`
+		AuthVersion          int      `json:"authVersion"`
+		DisableLP            bool     `json:"disableLP"`
+		DeversifiStarkKeyHex string   `json:"deversifiStarkKeyHex"`
+	} `json:"DVF"`
+	TokenBalancesHistory []string `json:"tokenBalancesHistory"`
+	TradingRewards       struct {
+		WeeklyAmount    int      `json:"weeklyAmount"`
+		RewardToken     string   `json:"rewardToken"`
+		ExcludedMarkets []string `json:"excludedMarkets"`
+	} `json:"tradingRewards"`
+	AmmPools      map[string]AmmPools      `json:"ammPools"`
+	TokenRegistry map[string]TokenRegistry `json:"tokenRegistry"`
+	IsRegistered  bool                     `json:"isRegistered"`
+	EthAddress    string                   `json:"ethAddress"`
+}
+
+// This method is used to register a Stark key that corresponds to an Ethereum public address. This will return deversifi Signature or DeversiFi application and user configuration details.
+func (p *Client) Register() (result *RegisterResponse, err error) {
+	s, publ, err := p.signAndPublicKey()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]string)
+	nonce := time.Now().Unix()
+	params["starkKey"] = publ
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+
+	fmt.Println(publ)
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/w/register", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	b, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(b))
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type GetUserBalancesResponse struct {
+}
+
+// This is used to retrieve the total and active balances of a user per token. Active balance is the balance that is currently available. Total balance (specified as balance) is the sum of all the balances including those locked for trading.
+func (p *Client) GetUserBalances() (result *GetUserBalancesResponse, err error) {
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]interface{})
+	nonce := time.Now().Unix()
+
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+	params["token"] = "ETH"
+	params["fields"] = []string{"balance", "updatedAt"}
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/r/getBalanceForUser/"+p.subaccount, jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	b, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(b))
+	/* err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	} */
 	return result, nil
 }
---
+++ order.go
@@ -0,0 +1,192 @@
+package dvfapi
+
+import (
+	"bytes"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type GetOrderResponse struct {
+	ID          string  `json:"_id"`
+	Symbol      string  `json:"symbol"`
+	Amount      float64 `json:"amount"`
+	Price       int     `json:"price"`
+	TotalFilled int     `json:"totalFilled"`
+	Pending     bool    `json:"pending"`
+	Canceled    bool    `json:"canceled"`
+	Active      bool    `json:"active"`
+}
+
+// This is endpoint is used to retrieve the details for a specific order using the order ID.
+func (p *Client) GetOrder(orderId string) (result []*GetOrderResponse, err error) {
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]string)
+	nonce := time.Now().Unix()
+	params["orderId"] = orderId
+	//params["cid"] = ""
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/r/getOrder", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type GetAllOrdersResponse []struct {
+	ID           string    `json:"_id"`
+	User         string    `json:"user"`
+	Symbol       string    `json:"symbol"`
+	Amount       float64   `json:"amount"`
+	TotalFilled  float64   `json:"totalFilled"`
+	Price        int       `json:"price"`
+	AveragePrice float64   `json:"averagePrice"`
+	FeeRate      string    `json:"feeRate"`
+	TokenBuy     string    `json:"tokenBuy"`
+	TotalBought  string    `json:"totalBought"`
+	TokenSell    string    `json:"tokenSell"`
+	TotalSold    string    `json:"totalSold"`
+	Active       bool      `json:"active"`
+	Type         string    `json:"type"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+	ActivatedAt  time.Time `json:"activatedAt"`
+}
+
+// This endpoints allows to retrieve details on all open orders.
+func (p *Client) GetAllOrders(base, quote string) (result *GetAllOrdersResponse, err error) {
+	var buffer bytes.Buffer
+	buffer.WriteString(base)
+	buffer.WriteString(":")
+	buffer.WriteString(quote)
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]string)
+	nonce := time.Now().Unix()
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+	params["symbol"] = buffer.String()
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/r/openOrders", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type CancelOrderResponse struct {
+	OrderId  string `json:"orderId"`
+	Canceled bool   `json:"canceled"`
+}
+
+// This endpoint allows to cancel a specific order.
+func (p *Client) CancelOrder(orderId string) (result *CancelOrderResponse, err error) {
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]string)
+	nonce := time.Now().Unix()
+	params["orderId"] = orderId
+	params["cid"] = ""
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/w/cancelOrder", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+type SubmitOrderResponse struct {
+}
+
+func (p *Client) SubmitOrder() (result *SubmitOrderResponse, err error) {
+	s, err := p.sign()
+	if err != nil {
+		return nil, err
+	}
+	params := make(map[string]interface{})
+	meta := make(map[string]interface{})
+	starkOrder := make(map[string]interface{})
+
+	nonce := time.Now().Unix()
+
+	params["cid"] = ""
+	params["type"] = ""
+	params["symbol"] = ""
+	params["amount"] = ""
+	params["price"] = ""
+
+	starkOrder["vaultIdSell"] = ""
+	starkOrder["vaultIdBuy"] = ""
+	starkOrder["amountSell"] = ""
+	starkOrder["amountBuy"] = ""
+	starkOrder["tokenSell"] = ""
+	starkOrder["tokenBuy"] = ""
+	starkOrder["nonce"] = strconv.FormatInt(nonce, 10)
+	starkOrder["expirationTimestamp"] = ""
+
+	meta["starkOrder"] = starkOrder
+	meta["starkMessage"] = ""
+	meta["ethAddress"] = ""
+	meta["starkPublicKey"] = ""
+	meta["starkSignature"] = ""
+
+	params["nonce"] = strconv.FormatInt(nonce, 10)
+	params["signature"] = s
+
+	jsonBody, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.sendRequest(http.MethodPost, "/v1/trading/w/cancelOrder", jsonBody, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decode(res, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
